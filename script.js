a8fd30aec71011a1073191079e9cd2ad95e4c07e
Rodney Carroll <rodcarroll63@gmail.com>
2026-05-18 16:05:06 -0500

    Final commit before adding to GitHub

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function addTask() {
 
   deleteBtn.onclick = () => {
     li.remove();
+    saveTasks();
   };
 
   li.appendChild(taskTextSpan);
@@ -24,7 +25,11 @@ function addTask() {
 
   document.getElementById('taskList').appendChild(li);
 
-  taskInput.value = '';
+  taskinput.value = '';
+  
+  // Save to localStorage
+  saveTasks();
+
 }
 
 /* connect button click to the function */
@@ -35,6 +40,47 @@ document
 /* allow adding task with Enter key */
 document
   .getElementById('taskInput')
-  .addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') addTask();
+  .addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      addTask();
+    }
   });
+
+  function saveTasks() {
+  const tasks = [];
+
+document.querySelectorAll('[data-testid="task-item"]').forEach((task) => {
+  const text = task.querySelector('[data-testid="task-text"]').textContent;
+  tasks.push(text);
+});
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  tasks.forEach((taskText) => {
+    const li = document.createElement('li');
+    li.setAttribute('data-testid', 'task-item');
+
+    const taskTextSpan = document.createElement('span');
+    taskTextSpan.textContent = taskText;
+    taskTextSpan.setAttribute('data-testid', 'task-text');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.setAttribute('data-testid', 'delete-task-btn');
+
+    deleteBtn.onclick = () => {
+      li.remove();
+      saveTasks();
+    };
+
+    li.appendChild(taskTextSpan);
+    li.appendChild(deleteBtn);
+
+    document.getElementById('taskList').appendChild(li);
+  });
+}
+window.addEventListener('load', loadTasks);
